cfdb9a687809b77b48c0cecf0906281352408b9f
Hidekazu Kobayashi <koba789@arkedgespace.com>
2023-11-14 05:20:13 +0000

    Update README

--- README.md
+++ README.md
@@ -4,19 +4,14 @@
 
 ### Prerequisites
 
+- Rust
 - Node.js (v18 or later)
 - yarn (v1.22 or later)
-
-### Install dependencies
-
-```console
-$ yarn
-```
 
 ### Run and open browser
 
 ```console
-$ yarn dev
+$ cargo run
 ```
 
 Now you can see the URL in the console. Open it in your browser.
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use clap::Parser;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 pub struct Args {
-    #[clap(long, env, default_value_t = Ipv4Addr::UNSPECIFIED.into())]
+    #[clap(long, env, default_value_t = Ipv4Addr::LOCALHOST.into())]
     addr: IpAddr,
     #[clap(long, env, default_value_t = 3000)]
     port: u16,
@@ -25,7 +25,7 @@ async fn main() {
     let app = Router::new().fallback(c2a_devtools::serve);
 
     let socket_addr = args.socket_addr();
-    println!("listening on {}", socket_addr);
+    println!("listening on http://{}", socket_addr);
     axum::Server::bind(&socket_addr)
         .serve(app.into_make_service())
         .await
